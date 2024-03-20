9f1b600e3356d3412ec7ad828a51b294351c77c3
Luisfergamerpro <114240163+Luisfergamerpro@users.noreply.github.com>
2024-03-20 19:33:53 -0400

    Add files via upload
    nothing, only practics

---
+++ green.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+int main(){
+    char unit;
+    float temp;
+    printf("\nIs the temperature in (F) or (*C)");
+    scanf("%c", &unit);
+
+    unit = toupper(unit);
+    if(unit == 'F'){
+        printf("\nEnter the temperature\n");
+         scanf("%f", &temp);
+         temp = (5.0/9.0) * (temp - 32);
+         printf("%f", temp); 
+    }
+    else if(unit == 'C'){
+        printf("\nEnter the temperature\n");
+        scanf("%f", &temp);
+        temp = (9.0/5.0 * temp) + 32;
+        printf("%.1f", temp);
+    }
+    else{
+        printf("\nThis character isn't valid\n");
+    }
+}
